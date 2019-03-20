add cyclic L1_Loss to training code, fix output colour calculation, merge data_cleaning and morphology

data_cleaning.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
 import shutil
@@ -22,16 +22,16 @@ def flip_img():
     # flipping original data
     for image in os.listdir(os.getcwd()):
         if image.endswith(".jpg"):
-            img = cv2.imread(image)
-            horizontal_img = cv2.flip(img, 1)
-            cv2.imwrite(str(i) + ".jpg", horizontal_img)
+            img = cv.imread(image)
+            horizontal_img = cv.flip(img, 1)
+            cv.imwrite(str(i) + ".jpg", horizontal_img)
             i += 1
 
 def resize_img(new_dimension):
     # os.chdir('data_uncleaned')
     for image in os.listdir(os.getcwd()):
         if image.endswith(".jpg"):
-            img = cv2.imread(image)
+            img = cv.imread(image)
             dim = 0, 0
             if img.shape[1] > img.shape[0]:  # A wide image
                 scale_percent = new_dimension / img.shape[0]  # percent of original size
@@ -45,17 +45,17 @@ def resize_img(new_dimension):
                 height = int(img.shape[0] * scale_percent)
                 width = new_dimension
                 dim = (width, height)
-            resized = cv2.resize(img, dsize=dim, interpolation=cv2.INTER_CUBIC)
-            cv2.imwrite(image, resized)
+            resized = cv.resize(img, dsize=dim, interpolation=cv.INTER_CUBIC)
+            cv.imwrite(image, resized)
 
-def crop_size_check(img):
-    if img.shape[0] == 224 and img.shape[1] == 224:
+def crop_size_check(img, size):
+    if img.shape[0] == size and img.shape[1] == size:
         return True
     else:
         return False
 
 
-def crop_img():
+def crop_img(new_dimension):
     cropped_id = 0
     if os.path.exists('../data/Real'):
         shutil.rmtree('../data/Real')
@@ -63,7 +63,7 @@ def crop_img():
 
     for image in os.listdir(os.getcwd()):
         if image.endswith(".jpg"):
-            img = cv2.imread(image)
+            img = cv.imread(image)
             height = img.shape[0]
             width = img.shape[1]
             cropped_img = np.empty
@@ -74,19 +74,19 @@ def crop_img():
                 crop_start = int(height / 2 - width / 2)
                 crop_end = int(crop_start + width)
                 cropped_img = img[crop_start:crop_end, 0:width]
-                if not crop_size_check(cropped_img):
+                if not crop_size_check(cropped_img, new_dimension):
                     print("{}, {}: {}*{}".format(image, cropped_id, height, width))
-                    cv2.resize(cropped_img, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
-                cv2.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
+                    cv.resize(cropped_img, dsize=(new_dimension,new_dimension), interpolation=cv.INTER_CUBIC)
+                cv.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
                 cropped_id += 1
             elif width < height * 1.9:
                 crop_start = int(width / 2 - height / 2)
                 crop_end = int(crop_start + height)
                 cropped_img = img[0:height, crop_start:crop_end]
-                if not crop_size_check(cropped_img):
+                if not crop_size_check(cropped_img, new_dimension):
                     print("{}, {}: {}*{}".format(image, cropped_id, height, width))
-                    cv2.resize(cropped_img, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
-                cv2.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
+                    cv.resize(cropped_img, dsize=(new_dimension,new_dimension), interpolation=cv.INTER_CUBIC)
+                cv.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
                 cropped_id += 1
             else:
                 crop_start = 0
@@ -94,10 +94,10 @@ def crop_img():
                 right_limit = img.shape[1]
                 while True:
                     cropped_img = img[0:height, crop_start:crop_end]
-                    if not crop_size_check(cropped_img):
+                    if not crop_size_check(cropped_img, new_dimension):
                         print("{}, {}: {}*{}".format(image, cropped_id, height, width))
-                        cv2.resize(cropped_img, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
-                    cv2.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
+                        cv.resize(cropped_img, dsize=(new_dimension,new_dimension), interpolation=cv.INTER_CUBIC)
+                    cv.imwrite("../data/Real/" + str(cropped_id) + ".jpg", cropped_img)
                     cropped_id += 1
                     crop_start += int(height * 0.8)
                     crop_end += int(height * 0.8)
@@ -105,8 +105,65 @@ def crop_img():
                         break
 
 
+def zero_out(img):
+
+    ret, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+
+    # ret, thresh1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+    # img = cv.normalize(img, None, 255, 0, cv.NORM_MINMAX, cv.CV_8UC1)
+    # Otsu's thresholding
+    #ret, thresh = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    # adaptive mean
+    # thresh4 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 5)
+    # thresh5 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 5)
+    # cv.imwrite("thresh1.jpg", thresh1)
+    #cv.imwrite("thresh2.jpg", thresh2)
+    # cv.imwrite("thresh3.jpg", thresh3)
+    # cv.imwrite("thresh4.jpg", thresh4)
+    # cv.imwrite("thresh5.jpg", thresh5)
+    return thresh
+
+# https://stackoverflow.com/questions/17815687/image-processing-implementing-sobel-filter
+def detect_edges(img):
+    #img = cv2.imread(imageSource)
+    #img = cv2.cvtColor(imageSource,cv2.COLOR_BGR2GRAY).astype(float)
+
+    edge_x = cv.Sobel(img,cv.CV_64F,1,0,ksize=3)
+    edge_y = cv.Sobel(img,cv.CV_64F,0,1,ksize=3)
+    edge = np.sqrt(edge_x**2 + edge_y**2)
+
+    #cv.imwrite("edge.jpg", edge)
+    return edge
+
+def rgb_to_gray(imageSource):
+    img = cv.imread(imageSource)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(float)
+    #cv.imwrite("gray.jpg", img)
+    return img
+
+def mor_closing():
+    generator_input_dir = '../../generator_input/edges'
+    if os.path.exists(generator_input_dir):
+        shutil.rmtree(generator_input_dir)
+    os.makedirs(generator_input_dir)
+
+    # flipping original data
+    for image in os.listdir(os.getcwd()):
+        if image.endswith(".jpg"):
+            img = rgb_to_gray(image)
+            kernel = np.ones((7, 7), np.uint8)
+            closing = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+            img = detect_edges(closing)
+            img = zero_out(img)
+            img = img.astype('uint8')
+            img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+            cv.imwrite(generator_input_dir + "/" + image, img)
+
 if __name__ == '__main__':
     rename_img()
-    resize_img(224)
-    crop_img()
+    resize_img(256)
+    crop_img(256)
     flip_img()
+    mor_closing()
+    pass
+

training.py
@@ -19,15 +19,15 @@ manualSeed = 999
 def get_data_loader(batch_size):
     # The output of torchvision datasets are PILImage images of range [0, 1].
     # We transform them to Tensors of normalized range [-1, 1].
-    transform_real = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    transform_edge = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
     # load each dataset with corresponding folders
-    realset = torchvision.datasets.ImageFolder(root='./data', transform=transform_real)
+    realset = torchvision.datasets.ImageFolder(root='./data', transform=transform)
     real_loader = torch.utils.data.DataLoader(realset, batch_size=batch_size,
                                                num_workers=1)
 
     # load each dataset with corresponding folders
-    edgeset = torchvision.datasets.ImageFolder(root='./generator_input', transform=transform_edge)
+    edgeset = torchvision.datasets.ImageFolder(root='./generator_input', transform=transform)
     edge_loader = torch.utils.data.DataLoader(edgeset, batch_size=batch_size,
                                                num_workers=1)
 
@@ -66,75 +66,84 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+# save model weights
+def get_model_name(name, batch_size, learning_rate, epoch):
+    """ Generate a name for the model consisting of all the hyperparameter values
+
+    Args:
+        config: Configuration object containing the hyperparameters
+    Returns:
+        path: A string with the hyperparameter name and value concatenated
+    """
+    path = "./checkpoints/model_{0}_bs{1}_lr{2}_epoch{3}".format(name,
+                                                   batch_size,
+                                                   learning_rate,
+                                                   epoch)
+    return path
 
 # https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
-def train(model, batch_size=32, learning_rate=1e-4, num_epochs=5):
+def train(model, batch_size=32, learning_rate=1e-4, L1_lambda=10, num_epochs=5):
     # clean the fake directory
     if os.path.exists('./data/Fake'):
         shutil.rmtree('./data/Fake')
     os.makedirs('./data/Fake')
+
+    if os.path.exists('./checkpoints'):
+        shutil.rmtree('./checkpoints')
+    os.makedirs('./checkpoints')
     
     # load training data
     real_loader, edge_loader = get_data_loader(batch_size)
 
     # loss function and optimizer
-    criterion = nn.BCELoss()
+    BCE_Loss = nn.BCELoss()
+    # MSE_Loss = nn.MSELoss()
+    L1_Loss = nn.L1Loss()
     optimizerD = optim.Adam(model.netD.parameters(), lr=learning_rate)
     optimizerG = optim.Adam(model.netG.parameters(), lr=learning_rate)
 
     real_label = 1
     fake_label = 0
 
-    # Lists to keep track of progress
-    img_list = []
-    G_losses = []
-    D_losses = []
-    iters = 0
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    test_edge = torchvision.datasets.ImageFolder(root='./test', transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_edge, batch_size=1,num_workers=1)
 
     print("Starting Training Loop...")
     # For each epoch
     for epoch in range(num_epochs):
         edge_iter = iter(edge_loader)
-        # For each batch in the dataloader
-        transform_edge = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor(),
-                                             transforms.Normalize((0.5,), (0.5,))])
 
-        test_edge = torchvision.datasets.ImageFolder(root='./test', transform=transform_edge)
-        test_loader = torch.utils.data.DataLoader(test_edge, batch_size=1,num_workers=1)
         test_fake = model.netG(next(iter(test_loader))[0])
-        test_fake = np.transpose(test_fake.detach().numpy().squeeze(), [1, 2, 0]) * 255
-        # print(test_fake.shape)
-        # print(test_fake)
+        test_fake = np.transpose(test_fake.detach().numpy().squeeze(), [1, 2, 0])
+        test_fake = (test_fake / 2 + 0.5) * 255
         cv.imwrite("./data/Fake/" + str(epoch) + ".jpg", test_fake)
 
         for i, real_data in enumerate(real_loader, 0):
-
+            ############################
+            # (1) Update D network
             ############################
             model.netD.zero_grad()
-            # Format batch
-            label = torch.full((batch_size,), real_label)
-            # Forward pass real batch through D
-            output = model.netD(real_data[0]).view(-1)
-            # Calculate loss on all-real batch
-            errD_real = criterion(output, label)
-            # Calculate gradients for D in backward pass
-            errD_real.backward()
-            D_x = output.mean().item()
-
             # Train with all-fake batch
             # Generate fake image batch with G
-            fake_batch = next(edge_iter)
-            fake = model.netG(fake_batch[0])
-            label.fill_(fake_label)
+            edge_batch = next(edge_iter)
+            fake = model.netG(edge_batch[0])
+
+            # Forward pass real batch through D
+            output = model.netD(edge_batch[0], real_data[0])
+            label = torch.full(output.shape, real_label)
+            # Calculate loss on all-real batch
+            loss_D_real = BCE_Loss(output, label)
+
             # Classify all fake batch with D
-            output = model.netD(fake.detach()).view(-1)
+            output = model.netD(edge_batch[0], fake.detach())
+            label = torch.full(output.shape, fake_label)
             # Calculate D's loss on the all-fake batch
-            errD_fake = criterion(output, label)
-            # Calculate the gradients for this batch
-            errD_fake.backward()
-            D_G_z1 = output.mean().item()
+            loss_D_fake = BCE_Loss(output, label)
+
             # Add the gradients from the all-real and all-fake batches
-            errD = errD_real + errD_fake
+            loss_D = (loss_D_real + loss_D_fake) / 2
+            loss_D.backward()
             # Update D
             optimizerD.step()
 
@@ -144,18 +153,28 @@ def train(model, batch_size=32, learning_rate=1e-4, num_epochs=5):
             model.netG.zero_grad()
             label.fill_(real_label)  # fake labels are real for generator cost
             # Since we just updated D, perform another forward pass of all-fake batch through D
-            output = model.netD(fake).view(-1)
+            output = model.netD(edge_batch[0], fake.detach())
             # Calculate G's loss based on this output
-            errG = criterion(output, label)
+            loss_G_BCE = BCE_Loss(output, label)
+            loss_G_L1 = L1_Loss(fake, real_data[0]) * L1_lambda
+            loss_G = loss_G_BCE + loss_G_L1
             # Calculate gradients for G
-            errG.backward()
-            D_G_z2 = output.mean().item()
+            loss_G.backward()
             # Update G
             optimizerG.step()
 
             # Output training stats
-            print("epoch: " + str(epoch) + ", iteration: " + str(i) + ", d_loss is: " + str(float(errD)) + ", g_loss is: " + str(float(errG)))
+            print("epoch: " + str(epoch) + ", iteration: " + str(i) + ", d_loss is: " + str(float(loss_D)) + ", g_loss is: " + str(float(loss_G)))
             print("=============================================================")
+        
+        # Save the current model (checkpoint) to a file
+        model_path = get_model_name(model.name, batch_size, learning_rate, epoch)
+        torch.save(model.state_dict(), model_path)
+
+    test_fake_result = model.netG(next(iter(test_loader))[0])
+    test_fake_result = np.transpose(test_fake_result.detach().numpy().squeeze(), [1, 2, 0]) * 255
+    test_fake_result = (test_fake_result / 2 + 0.5) * 255
+    cv.imwrite("./data/Fake/result.jpg", test_fake_result)
 
 
 ###############################################################################
@@ -165,11 +184,10 @@ if __name__ == '__main__':
     gan = DCGAN(filter_size)
     gan.netG.apply(weights_init)
     gan.netD.apply(weights_init)
-    num_epoch = 5
+    num_epoch = 10
     batch_size = 32
     learning_rate = 1e-4
-    train(gan, batch_size, learning_rate, num_epoch)
-    #
-    # print images
-    #real_loader, edge_loader = get_data_loader(1)
-    #train_model(gan)
+    L1_lambda = 10
+    train(gan, batch_size, learning_rate, L1_lambda, num_epoch)
+    pass
+